c5cba96a958d9e02f3f4c6fc9264e93d00767068
Mayuri Mahto <89357682+mayurimahto@users.noreply.github.com>
2024-07-13 11:54:38 +0530

    Create Q7.cpp

---
+++ Q7.cpp
@@ -0,0 +1,25 @@
+//Write a function to compute the sum of the digits of a given number.The function should return the sum of the digits of the number.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class gfg {
+public:
+	int getSum(int n)
+	{
+		int sum = 0;
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+		return sum;
+	}
+};
+
+int main()
+{
+	gfg g;
+	int n = 687;
+	cout << g.getSum(n);
+	return 0;
+}
