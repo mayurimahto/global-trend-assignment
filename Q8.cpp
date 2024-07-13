68fe90ff698d413aa402611bf1c0555597ec6a62
Mayuri Mahto <89357682+mayurimahto@users.noreply.github.com>
2024-07-13 11:56:02 +0530

    Create Q8.cpp

---
+++ Q8.cpp
@@ -0,0 +1,24 @@
+//Write a function to find the greatest common divisor (GCD) of two numbers. The function should return the GCD of a and b.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int result = min(a, b);
+    while (result > 0) {
+        if (a % result == 0 && b % result == 0) {
+            break;
+        }
+        result--;
+    }
+    return result;
+}
+
+int main()
+{
+    int a = 98, b = 56;
+    cout << "GCD of " << a << " and " << b << " is "
+         << gcd(a, b);
+    return 0;
+}
